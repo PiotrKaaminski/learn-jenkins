fc3adf820d6d1d81b54a64d403185eecbeee0bd2
Piotr Kamiński <piotr.kaminski@billongroup.com>
2021-07-12 12:20:18 +0200

    modify Jenkinsfile

--- listAvailableProfiles.groovy
+++ listAvailableProfiles.groovy
@@ -1,5 +1,5 @@
 def availableProfiles() {
-    String profiles = 'mvn help:all-profiles | grep "Profile Id:" | awk "{print $3}" | tr "\n" " "'.execute().text
+    def profiles = 'mvn help:all-profiles | grep "Profile Id:" | awk \'{print $3}\' | tr "\n" " "'.execute().text
     return profiles
 }
 
