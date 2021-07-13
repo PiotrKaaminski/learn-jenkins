46bd21c49d88e2bc76ff8b9d39b3f4edad5b9673
Piotr Kamiński <piotr.kaminski@billongroup.com>
2021-07-13 14:50:39 +0200

    modify Jenkinsfile

--- readProfiles.sh
+++ readProfiles.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 #mvn help:all-profiles | grep "Profile Id:" | awk '{print $3}' | tr '\n' ','
 ARRAYS=$(mvn help:all-profiles | grep "Profile Id:\|Listing Profiles" | awk '{print $3}' | sed  "s/Profiles//" | sed "s/for//")
-echo $ARRAYS
+echo "$ARRAYS"
