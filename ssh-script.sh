5b5e232342ecb6c6601aec679e95c8065979d0be
Piotr Kamiński <piotr.kaminski@billongroup.com>
2021-09-15 15:12:54 +0200

    modify Jenkinsfile

--- ssh-script.sh
+++ ssh-script.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 echo "my script"
 #git clone git@morty.billongroup.com:durable-media/bem/bem-common
-#cd bem-common
+#cd bem-common/dm-model/dm-db-chm
+ssh -N -L 12345:10.0.6.21:12345 -N piotrk@10.0.6.21 & pid=$!
+echo "killing ssh tunnel $pid"
 
 
 #ssh -i /home/pkaminski/.ssh/new-key -l jenkins 10.0.6.21 "
 #  echo 'hello from jenkins' > test.txt
 #"
-#rm -r bem-common
+rm -r bem-common
