07e0b76217c2f8db80abd90861b39803e56ceb00
Piotr Kamiński <piotr.kaminski@billongroup.com>
2021-10-01 10:10:53 +0200

    modify Jenkinsfile

--- ssh-script.sh
+++ ssh-script.sh
@@ -5,7 +5,7 @@ cd bem-common/dm-model/dm-db-chm
 #ssh -N -L 5431:10.0.6.21:5432 -N piotrk@10.0.6.21 & pid=$!
 #echo "killing ssh tunnel $pid"
 
-mvn clean install liquibase:dropAll liquibase:update -Dliquibase.contexts=all,mvp,agreements
+#mvn clean install liquibase:dropAll liquibase:update -Dliquibase.contexts=all,mvp,agreements
 
 # script for ssh connection with script on server side
 #ssh -i /home/pkaminski/.ssh/new-key -l jenkins 10.0.6.21 "
